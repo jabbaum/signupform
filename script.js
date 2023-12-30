f294f53dcca0aab9d83d289b363cd49bdc269d61
jabbaum <jabbaum@gmail.com>
2023-12-30 13:22:00 -0800

    set up functionality of checking if pass2 matched pass 1 and presents a ui error if it fails validation

---
+++ script.js
@@ -0,0 +1,20 @@
+let pass1 = document.querySelector('#pass');
+let pass2 = document.querySelector('#pass2');
+
+pass2.addEventListener("input", (e)=>{
+    let isValid = 0;
+    let currentPass1 = pass1.value;
+    let currentPass2 = pass2.value;
+    let errorDisplayer = document.querySelector('label>span');
+
+    isValid = currentPass2.length === 0 || currentPass2 === currentPass1;
+    console.log(`here is pass1: ${currentPass1} and here is pass2: ${currentPass2   } and here is valid: ${isValid} lastly, here is curpass2.length === 0: ${currentPass2.length===0}`);
+
+    if(isValid) {
+        errorDisplayer.textContent = '';
+        pass2.className = "valid";
+    } else {
+        errorDisplayer.textContent = ' Passwords must match';
+        pass2.className = 'invalid'
+    }
+});
