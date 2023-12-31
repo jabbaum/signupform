3012e2e73110e505348f9d1425e262ffed87a0f6
jabbaum <jabbaum@gmail.com>
2023-12-30 17:37:14 -0800

    made it so that changing either the first or the matching password show the helpful ui prompt and invalid nature of the fields

--- script.js
+++ script.js
@@ -2,26 +2,38 @@ let pass1 = document.querySelector('#pass');
 let pass2 = document.querySelector('#pass2');
 
 function passMatch(origin, match) {
-    isValid = origin === match;
+    let isValid = origin === match;
+    console.log(`Here is what is recieved: ${origin} and ${match} and here is valid: ${isValid}`);
+    let errorDisplayer = document.querySelectorAll('label>span');
+
     if(isValid) {
-        
+        errorDisplayer.forEach(element => {
+            element.textContent = '';
+        });
+        pass2.className = "valid";
+        pass1.className = "valid";
+        return true;
+    } else {
+        errorDisplayer.forEach(element => {
+            element.textContent = ' Passwords must match';
+        });
+        pass2.className = 'invalid';
+        pass1.className = 'invalid';
+        return false;
     }
 }
 
 pass2.addEventListener("input", (e)=>{
-    let isValid = 0;
     let currentPass1 = pass1.value;
     let currentPass2 = pass2.value;
-    let errorDisplayer = document.querySelector('label>span');
+    console.log(`here is what is passed to the function: ${currentPass1} ${currentPass2}`);
 
-    isValid = currentPass2.length === 0 || currentPass2 === currentPass1;
-    console.log(`here is pass1: ${currentPass1} and here is pass2: ${currentPass2   } and here is valid: ${isValid} lastly, here is curpass2.length === 0: ${currentPass2.length===0}`);
+    passMatch(currentPass1, currentPass2);
+});
 
-    if(isValid) {
-        errorDisplayer.textContent = '';
-        pass2.className = "valid";
-    } else {
-        errorDisplayer.textContent = ' Passwords must match';
-        pass2.className = 'invalid'
-    }
+pass1.addEventListener("input", (e) => {
+    let currentPass1 = pass1.value;
+    let currentPass2 = pass2.value;
+
+    passMatch(currentPass1, currentPass2);
 });
